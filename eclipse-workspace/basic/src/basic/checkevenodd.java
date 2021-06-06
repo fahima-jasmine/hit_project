bda653149adc5d7ce52cd89750f0baee8a23b42b
Admin <Admin@PERSONAL.mshome.net>
2021-06-06 16:34:38 +0530

    [java project]lab exercise

---
+++ eclipse-workspace/basic/src/basic/checkevenodd.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class checkevenodd {
+	 public static void main(String args[])   {  
+		 int num=1;    
+     if ( num % 2 == 0 )   
+    	 System.out.println("Entered number is even"); 
+     else 
+    	 System.out.println("Entered number is odd"); 
+	 }
+
+}
